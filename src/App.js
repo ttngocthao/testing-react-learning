e3eef099d583063d4df79d859aa883b6f87089d8
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-12-20 20:27:12 +0000

    second app init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Learn react testing</h1>
+      <h1>Guess word Game</h1>
     </div>
   );
 }
