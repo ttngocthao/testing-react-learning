d57b7595ab28b827ddde2ddaee5e1c4fa0a4e763
ttngocthao <ttngocthao_87@yahoo.com>
2020-12-22 17:33:43 +0000

    added tests words guessed - not completed yet

--- src/components/guessedWords/GuessedWords.js
+++ src/components/guessedWords/GuessedWords.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const GuessedWords = (props) => {   
     return (
         <div data-test='component-guessed-words' >
-           {props.guessedWords.length===0 ? <h2 data-test='instruction-msg'>Try to guess the secret word</h2>:<h2>Table of guessed words here</h2>}
+           {props.guessedWords.length===0 ? <h2 data-test='instruction-msg'>Try to guess the secret word!</h2>:<h2>Table of guessed words here</h2>}
         </div>
     )
 }
--- src/components/guessedWords/GuessedWords.test.js
+++ src/components/guessedWords/GuessedWords.test.js
@@ -36,5 +36,26 @@ describe('if there is no words guessed',()=>{
 })
 
 describe('if there are words guessed',()=>{
-    
+    const guessedWords=[
+        {guessedWord: 'train',letterMatchCount: 3},
+        {guessedWord: 'party',letterMatchCount: 5},
+        {guessedWord: 'angle',letterMatchCount: 1}
+    ]
+    let wrapper
+    beforeEach(()=>{
+        wrapper = setup({guessedWords})
+    })
+    test('renders without errors',()=>{
+        const guessedWordsComp = findByTestAttr(wrapper,'component-guessed-words')
+        expect(guessedWordsComp.length).toBe(1)
+    })
+    test('renders guessed word list',()=>{
+        const guessedWordsList = findByTestAttr(wrapper,'list-guessed-words')
+        expect(guessedWordsList.length).toBe(1)
+    })
+    test('correct number of guessed words',()=>{
+        const guessedWord = findByTestAttr(wrapper,'guessed-word')
+        expect(guessedWord.length).toBe(guessedWords.length)
+    })
+    test('not render instruction',()=>{})
 })
