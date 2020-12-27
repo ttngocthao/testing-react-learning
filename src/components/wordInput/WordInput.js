7766634232e12d1296104b0da45535ee20cbd42c
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2020-12-27 16:09:55 +0000

    added input component and its test

--- src/components/wordInput/WordInput.js
+++ src/components/wordInput/WordInput.js
@@ -1,11 +1,15 @@
 import React from "react";
-
-const WordInput = () => {
+import PropTypes from "prop-types";
+const WordInput = ({ secretWord }) => {
   return (
     <div data-test="component-input">
       <h2>Your guess is</h2>
     </div>
   );
+};
+
+WordInput.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default WordInput;
--- src/components/wordInput/wordInput.test.js
+++ src/components/wordInput/wordInput.test.js
@@ -1,12 +1,17 @@
 import { shallow } from "enzyme";
 import WordInput from "./WordInput";
-import { findByTestAttr } from "../../../test/testUtils";
-const setup = () => {
-  return shallow(<WordInput />);
+import { findByTestAttr, checkProps } from "../../../test/testUtils";
+
+const setup = (secretWord = "party") => {
+  return shallow(<WordInput secretWord={secretWord} />);
 };
 
 test("renders word input without error", () => {
   const wrapper = setup();
   const componentInput = findByTestAttr(wrapper, "component-input");
   expect(componentInput.length).toBe(1);
+});
+
+test("does not throw warning with expected props", () => {
+  checkProps(WordInput, { secretWord: "party" });
 });
